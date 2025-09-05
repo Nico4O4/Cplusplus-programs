12beb0d25844e0fc393d5de6357d2eaa1857a22e
Nico4O4 <nicopfin@bluewin.ch>
2025-09-05 23:10:40 +0200

    if / else exercise

---
+++ if_else/if_else/if_else.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std; //damit man nur cout schreiben muss
+
+int main()
+{
+    int first_number = 100;
+    int second_number = 2;
+
+    cout << "Is the first number greater than the second number?\n\n";
+
+    if (first_number > second_number)
+        cout << "Yess :p\n";
+
+    else
+        cout << "nope :D\n";
+
+
+    return 0;
+}
+
