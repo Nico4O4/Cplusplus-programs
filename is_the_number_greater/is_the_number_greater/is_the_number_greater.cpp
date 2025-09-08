16e3f16e99d188f17738e426117a3395264c37a5
Nico4O4 <nicopfin@bluewin.ch>
2025-09-08 22:06:15 +0200

    finally fixed this ONE bug...

--- is_the_number_greater/is_the_number_greater/is_the_number_greater.cpp
+++ is_the_number_greater/is_the_number_greater/is_the_number_greater.cpp
@@ -6,29 +6,29 @@ int main() {
     int first_number;
     int second_number;
 
-    cout << "Enter the first number" << "\n -> ";
+    cout << "Enter the first number: ";
     cin >> first_number;
 
-    cout << "Enter the second number" << "\n -> ";
+    cout << "Enter the second number: ";
     cin >> second_number;
 
 
-    if (first_number > second_number) {
-        cout << "Ur first number is greater!";
+    if (first_number > second_number)//erste zahl grösser als >
+    {
+        cout << "Ur first number is greater";
     }
 
 
-
-    else (first_number < second_number) {
-        cout << "Ur first number is greater!";
+    else if (first_number < second_number)//zweite zahl grösser als <
+    {
+        cout << "Ur second number is greater";
     }
 
 
-
-    else if (first_number == second_number); {
-        cout << "Both numbers are equal :p";
+    else //else hat NIEMALS eine bedingung
+    {
+        cout << "Both numbers are qeual :p";
     }
-    
    
 
 
