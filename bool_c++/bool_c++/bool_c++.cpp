a07c54e97d77708785f508791238adcf65f519fc
Nico4O4 <nicopfin@bluewin.ch>
2025-09-15 21:36:11 +0200

    first time with bool

---
+++ bool_c++/bool_c++/bool_c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	//bool = immer True / False = 1 / 0
+
+	bool c_plus = true;
+	bool css = false;
+
+	cout << "Is C++ fun? = " << c_plus << " Is CSS fun? = " << css;
+
+	
+	return 0;
+}
